7377bcc99b743b9da800f6805c3b15cc6da41ac0
Denys <Melnik.Denys99@gmail.com>
2024-08-06 14:13:33 +0300

    hw done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,70 @@
-import "./App.css";
-import userData from './userData.json';
-import friends from './friends.json';
-import transactions from "./transactions.json";
-import Profile from "./components/Profile/Profile";
-import FriendList from './components/FriendList/FriendList'
-import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+
+import Description from './components/Description/Description';
+import Options from './components/Options/Options';
+import Feedback from './components/Feedback/Feedback';
+import Notification from './components/Notification/Notification';
+
+import './App.css';
+
+import { useState, useEffect } from 'react';
 
 const App = () => {
-  return (
-    <>
-           {" "}
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-         {" "}
+  const initialFeedbackValues = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
+  const getFeedbackFromLS = () => {
+    const savedFeedback = window.localStorage.getItem('saved-feedback');
+    return savedFeedback !== null
+      ? JSON.parse(savedFeedback)
+      : initialFeedbackValues;
+  };
+
+  const [feedbacks, setFeedbacks] = useState(getFeedbackFromLS);
+
+  useEffect(() => {
+    window.localStorage.setItem('saved-feedback', JSON.stringify(feedbacks));
+  }, [feedbacks]);
+
+  const updateFeedback = feedbackType => {
+    setFeedbacks({
+      ...feedbacks,
+      [feedbackType]: feedbacks[feedbackType] + 1,
+    });
+  };
+
+  const totalFeedback = feedbacks.good + feedbacks.neutral + feedbacks.bad;
+
+  const resetFeedback = () => {
+    setFeedbacks(initialFeedbackValues);
+  };
+  const positiveFeedback = Math.round(
+    ((feedbacks.good + feedbacks.neutral) / totalFeedback) * 100
+  );
+
+  return (
+    <div className="appContainer">
+      <Description />
+      <Options
+        options={['good', 'neutral', 'bad']}
+        onOptionBtnClick={updateFeedback}
+        totalFeedback={totalFeedback}
+        onResetBtnClick={resetFeedback}
+      />
+      {totalFeedback ? (
+        <Feedback
+          goodFeedback={feedbacks.good}
+          neutralFeedback={feedbacks.neutral}
+          badFeedback={feedbacks.bad}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
   );
 };
 
